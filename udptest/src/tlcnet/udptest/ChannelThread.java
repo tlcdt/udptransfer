5522d939c7f27c65bb0ceb354affdec7bca7bfc6
andrea-dittadi <andrea.dittadi@gmail.com>
2014-12-16 12:20:31 +0100

    Implemented non-blocking random packet delay: a packet is assigned a delay time, and the transmission will occur asynchronously, while the reception of packets can go on in the main thread.

--- udptest/src/tlcnet/udptest/ChannelThread.java
+++ udptest/src/tlcnet/udptest/ChannelThread.java
@@ -6,11 +6,15 @@ import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketException;
 import java.util.Arrays;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 public class ChannelThread {
 	private static final double FIXED_DROP_PROB = 0.25;  // TODO: temp probability and delay
 	private static final int MIN_DELAY_MS = 10;
 	private static final int MAX_DELAY_MS = 60;
+	
+	private static final int CORE_POOL_SIZE = 50;
 	
 	private static final int RX_BUFSIZE = 2048; // exceeding data will be discarded
 	private static final int INVALID_PORT = -1;
@@ -42,83 +46,103 @@ public class ChannelThread {
 			System.err.println("Error creating a datagram socket:\n" + e);
 			listenSocket.close(); System.exit(-1);
 		}
+
 		
 		
-		// ---- MAIN LOOP ----
+		// * * * *  MAIN LOOP  * * * *
+		
 		while(true) {
-			forward(listenSocket, outSocket);
-		}
-	}
+			
+			ScheduledThreadPoolExecutor schedExec = new ScheduledThreadPoolExecutor(CORE_POOL_SIZE);
+			
+			
+			// ---- Receive packet ----
+			byte[] recvBuf = new byte[RX_BUFSIZE];
+			DatagramPacket recvPkt = new DatagramPacket(recvBuf, recvBuf.length);
+			try{
+				listenSocket.receive(recvPkt);
+			}
+			catch(IOException e) {
+				System.err.println("I/O error while receiving datagram:\n" + e);
+				listenSocket.close(); outSocket.close(); System.exit(-1);
+			}
+			
+			
+			// ---- Process received packet and prepare new packet ----
+			byte[] recvData = recvPkt.getData();				// payload of recv UDP datagram
+			recvData = Arrays.copyOf(recvData, recvPkt.getLength());
+			UTPpacket recvUTPpkt = new UTPpacket(recvData);		// parse UDP payload
+			InetAddress dstAddr = recvUTPpkt.dstAddr;			// get intended dest address and port
+			int dstPort = (int)recvUTPpkt.dstPort & 0xffff;
+			InetAddress srcAddr = recvPkt.getAddress();			// get sender address and port (UDP header)
+			int srcPort = recvPkt.getPort();
+			
+			UTPpacket sendUTPpkt = new UTPpacket();	// Create new UTP packet (it will be the payload of the datagram)
+			sendUTPpkt.function = recvUTPpkt.function;
+			sendUTPpkt.sn = recvUTPpkt.sn;
+			sendUTPpkt.payl = recvUTPpkt.payl;
+			 // - The following two lines are needed only to notify the TRUE sender (DS or DR) to the
+			 //   receiver (DR or DS); for other implementations of DR they are harmless.
+			 //   Anyway, our DR will not be compatible with a Channel implemented without these lines.
+			 //	  TODO What should we do? 
+			sendUTPpkt.dstAddr = srcAddr; 			// Destination address (and port) from the destination POV: now,
+			sendUTPpkt.dstPort = (short)srcPort;	//  for the channel, it's the addr (and port) of the sender.
+			 // - - - - - -
+			byte[] sendData = sendUTPpkt.getRawData(); 	// payload of outgoing UDP datagram
+			
+			//DEBUG
+			System.out.println("\n------\nRECV DATA:\n" + Utils.byteArr2str(recvData));
+			
+			System.out.println("Rcvd SN=" + recvUTPpkt.sn + "\nPayload (len=" + recvUTPpkt.payl.length
+							+ "): " + new String(recvUTPpkt.payl) + "\n");
 
+			
+			
+			// ---- Send packet ----
+			
+			if (mustDrop(sendUTPpkt.payl.length)) {
+				System.out.println("Dropping packet SN=" + recvUTPpkt.sn + " towards " + dstAddr.getHostAddress());
+				continue;
+			}
+			DatagramPacket sendPkt = new DatagramPacket(sendData, sendData.length, dstAddr, dstPort);
+			schedExec.schedule(new SendDelayedPacket(outSocket, listenSocket, sendPkt), getRndDelay(), TimeUnit.MILLISECONDS);
 
-	private void forward(DatagramSocket srcSock, DatagramSocket dstSock) {
-		
-		// ---- Receive packet ----
-		byte[] recvBuf = new byte[RX_BUFSIZE];
-		DatagramPacket recvPkt = new DatagramPacket(recvBuf, recvBuf.length);
-		try{
-			srcSock.receive(recvPkt);
-		}
-		catch(IOException e) {
-			System.err.println("I/O error while receiving datagram:\n" + e);
-			srcSock.close(); dstSock.close(); System.exit(-1);
-		}
-		
-		
-		// ---- Process received packet and prepare new packet ----
-		byte[] recvData = recvPkt.getData();				// payload of recv UDP datagram
-		recvData = Arrays.copyOf(recvData, recvPkt.getLength());
-		UTPpacket recvUTPpkt = new UTPpacket(recvData);		// parse UDP payload
-		InetAddress dstAddr = recvUTPpkt.dstAddr;			// get intendend dest address and port
-		int dstPort = (int)recvUTPpkt.dstPort & 0xffff;
-		InetAddress srcAddr = recvPkt.getAddress();			// get sender address and port (UDP header)
-		int srcPort = recvPkt.getPort();
-		
-		UTPpacket sendUTPpkt = new UTPpacket();	// Create new UTP packet (it will be the payload of the datagram)
-		sendUTPpkt.function = recvUTPpkt.function;
-		sendUTPpkt.sn = recvUTPpkt.sn;
-		sendUTPpkt.payl = recvUTPpkt.payl;
-		 // - The following two lines are needed only to notify the TRUE sender (DS or DR) to the
-		 //   receiver (DR or DS); for other implementations of DR they are harmless.
-		 //   Anyway, our DR will not be compatible with a Channel implemented without these lines.
-		 //	  TODO What should we do? 
-		sendUTPpkt.dstAddr = srcAddr; 			// Destination address (and port) from the destination POV: now,
-		sendUTPpkt.dstPort = (short)srcPort;	//  for the channel, it's the addr (and port) of the sender.
-		 // - - - - - -
-		byte[] sendData = sendUTPpkt.getRawData(); 	// payload of outgoing UDP datagram
-		
-		//DEBUG
-		System.out.println("\n------\nRECV DATA:\n" + Utils.byteArr2str(recvData));
-		
-		System.out.println("Rcvd SN=" + recvUTPpkt.sn + "\nPayload (len=" + recvUTPpkt.payl.length
-						+ "): " + new String(recvUTPpkt.payl) + "\n");
-
-		
-		
-		// ---- Send packet ----
-		if (mustDrop(sendUTPpkt.payl.length)) {
-			System.out.println("Dropping packet SN=" + recvUTPpkt.sn + " towards " + dstAddr.getHostAddress());return;
-		}
-		try {
-			Thread.sleep(getRndDelay()); // TODO: WRONG.
-		} catch (InterruptedException e) {
-			//e1.printStackTrace();
-		}
-		DatagramPacket sendPkt = new DatagramPacket(sendData, sendData.length, dstAddr, dstPort);
-		try {
-			dstSock.send(sendPkt);
-		}
-		catch(IOException e) {
-			System.err.println("I/O error while sending datagram:\n" + e);
-			dstSock.close(); srcSock.close(); System.exit(-1);
 		}
 	}
 	
+	
+	private class SendDelayedPacket implements Runnable {
+		
+		private DatagramSocket dstSock;
+		private DatagramSocket srcSock;
+		private DatagramPacket sendPkt;
+
+		SendDelayedPacket(DatagramSocket dstSock, DatagramSocket srcSock, DatagramPacket sendPkt) {
+			super();
+			this.dstSock = dstSock;
+			this.srcSock = srcSock;
+			this.sendPkt = sendPkt;
+		}
+
+
+		@Override
+        public void run() {
+        	
+    		try {
+    			dstSock.send(sendPkt);
+    		}
+    		catch(IOException e) {
+    			System.err.println("I/O error while sending datagram:\n" + e);
+    			dstSock.close(); srcSock.close(); System.exit(-1);
+    		}
+        }
+    }
+
 
 	// TODO: For now it's just a Bernoulli
 	private boolean mustDrop(int length) {
 		if (length == 0)
-			return false; // TODO: for now empty packets are always passed on
+			return false; // TODO: for now empty packets (for instance ACKs) are always passed on
 		if (Math.random() < FIXED_DROP_PROB)
 			return true;
 		return false;
