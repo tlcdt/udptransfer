3edce7cb330d583bff6ddc443fbe6e2c539a7589
andrea <andrea.dittadi@gmail.com>
2014-12-30 23:43:49 +0100

    (a new class was missing)

---
+++ udptest/src/tlcnet/udptest/DuplicateIdHandler.java
@@ -0,0 +1,99 @@
+package tlcnet.udptest;
+
+import java.util.Arrays;
+
+public class DuplicateIdHandler {
+	private static final int WNDSIZE = 10000;
+	private static final int WNDSHIFT= 1000;
+	
+	//private int wndSize;
+	private int wndShift;
+	private int left; // left margin
+	private int right;
+	private boolean[] memory;
+	private int misses = 0;
+
+	
+	
+	
+	
+	/**
+	 * @param wndSize
+	 * @param wndShift
+	 * @param left
+	 */
+	public DuplicateIdHandler(int wndSize, int wndShift, int left) {
+		super();
+		//this.wndSize = wndSize;
+		this.wndShift = wndShift;
+		this.left = left;
+		right = left + wndSize;
+		memory = new boolean[wndSize];
+	}
+
+	
+	
+	
+	
+	
+	/**
+	 * @param wndSize
+	 * @param wndShift
+	 */
+	public DuplicateIdHandler(int wndSize, int wndShift) {
+		this(wndSize, wndShift, 1);
+	}
+
+
+	
+
+	/**
+	 * 
+	 */
+	public DuplicateIdHandler() {
+		this(WNDSIZE, WNDSHIFT, 1);		
+	}
+
+
+
+
+
+
+	public boolean isNew(int id) {
+		if(id < left) {
+			misses++;
+			return true; // we forgot... assume it's new
+		}
+
+		if(id > right) {
+			shiftWindow();
+			setElement(id);
+			return true;
+		}
+
+		boolean result = !getElement(id);
+		setElement(id);
+		return result;
+	}
+
+	private void setElement(int id) {
+		memory[id - left] = true;
+	}
+
+	private boolean getElement(int id) {
+		return memory[id - left];
+	}
+
+
+	private void shiftWindow() {
+		System.arraycopy(memory, wndShift, memory, 0, memory.length - wndShift);
+		Arrays.fill(memory, memory.length - wndShift, memory.length, false);
+		left += wndShift;
+		right += wndShift;
+	}
+
+
+	public int misses() {
+		return misses;
+	}
+}
